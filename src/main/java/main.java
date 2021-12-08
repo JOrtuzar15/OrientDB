e73a532129ebadfb5553502d7d2874562cfbe452
JOrtuzar15 <jortuzar6@gmail.com>
2021-12-08 19:16:37 +0100

    feat: funciona en remoto

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,18 +1,19 @@
-import com.orientechnologies.orient.core.db.ODatabaseSession;
 import com.orientechnologies.orient.core.db.ODatabaseType;
 import com.orientechnologies.orient.core.db.OrientDB;
+import com.orientechnologies.orient.core.db.OrientDBConfig;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.metadata.schema.OClass;
 import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.OVertex;
 
 public class main {
     public static void main(String[] args){
-        OrientDB orient = new OrientDB("remote:localhost","root","rootpwd",null);
+        OrientDB orient = new OrientDB("remote:172.17.0.2:2424","root","rootpwd", OrientDBConfig.defaultConfig());
 
         if (!orient.exists("database_as")) {
             orient.create("database_as", ODatabaseType.PLOCAL);
         }
-        ODatabaseSession db = orient.open("database_as","root","rootpwd");
+        ODatabaseDocument db = orient.open("database_as","root","rootpwd");
 
         OClass persona = db.getClass("Persona");
         if (persona == null) {
